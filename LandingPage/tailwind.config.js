003cdb2ae83abed1fde5b71278a64f4bc4e29790
oat47110 <33687611+SrnDev16@users.noreply.github.com>
2023-11-02 17:26:09 +0700

    update
    first section.

--- LandingPage/tailwind.config.js
+++ LandingPage/tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     extend: {
       fontFamily: {
         'primary': ['Poppins']
-      } 
+      },
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
+        bluePrimary:{
+          100: "#28688d"
+        }
+      }
     },
   },
   plugins: [],
